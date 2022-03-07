71b29d4d96243a847207583ba40b00a477d888b6
gyubum <sgb8170@naver.com>
2022-03-07 23:29:27 +0900

    React Router 스위치 기능

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,8 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Container, Nav, NavDropdown, Jumbotron, Button} from 'react-bootstrap';
+import { useState } from 'react';
+import Data from './data.js';
+import Detail from './Detail.js';
+
+import { Link, Route, Switch} from 'react-router-dom';
 
 function App() {
+
+    let [shoes, shoes변경] = useState(Data);
+
     return (
         <div className="App">
             <Navbar bg="light" expand="lg">
@@ -12,8 +20,8 @@ function App() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link> <Link to="/">Home</Link></Nav.Link>
+                            <Nav.Link> <Link to={"/detail"}>Detail</Link></Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -26,39 +34,63 @@ function App() {
                 </Container>
             </Navbar>
 
-            <Jumbotron className='background'>
-                <h1>20% Season Off</h1>
-                <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling extra
-                    attention to featured content or information.
-                </p>
-                <p>
-                    <Button variant="primary">Learn more</Button>
-                </p>
-            </Jumbotron>
+        <Switch>
+            
+            <Route exact path="/">
+                <Jumbotron className='background'>
+                    <h1>20% Season Off</h1>
+                    <p>
+                        This is a simple hero unit, a simple jumbotron-style component for calling extra
+                        attention to featured content or information.
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </Jumbotron>
+                <div className='container'>
+                    <div className='row'>
+                        
+                        {
+                            shoes.map(function(a,i){
+                                return(
+                                    <Compo 글제목={shoes[i].title} 글내용={shoes[i].content} index={i}></Compo>
+                                );
+                            })
 
-            <div className='container'>
-              <div className='row'>
-                <div className='col-md-4'>
-                  <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-                  <h4>상품명</h4>
-                  <p>상품설명 & 가격</p>
+                        }
+
+                    </div>
                 </div>
-                <div className='col-md-4'>
-                  <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
-                  <h4>상품명</h4>
-                  <p>상품설명 & 가격</p>  
-                </div>
-                <div className='col-md-4'>
-                  <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
-                  <h4>상품명</h4>
-                  <p>상품설명 & 가격</p>  
-                </div>
+            </Route>
+
+
+            <Route path="/detail">
+                <Detail></Detail>
+            </Route>
+
+            <Route path={"/:id"}> 
+                <div>1234</div>
+            </Route>
+
+        </Switch>    
+
                 
-              </div>
-            </div>
+
+            
         </div>
     );
 }
+
+function Compo(props){
+    return(
+        <div className='col-md-4'>
+                  <img src={"https://codingapple1.github.io/shop/shoes"+ (props.index+1) +".jpg"} width="100%" />
+                  <h4>{ props.글제목 }</h4>
+                  <p>{ props.글내용}</p>
+                </div>
+    );
+} 
+
+
 
 export default App;
